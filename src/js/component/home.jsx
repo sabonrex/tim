85e41d9bfe1b3ce759686f4416e1f3d789994cb7
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-03-04 01:27:38 +0000

    try

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,20 @@
 import React from "react";
+import Digit from "./digit.jsx";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="wrapper">
+      <Digit digit={Math.floor(props.time / 100000) % 100000} />
+      <Digit digit={Math.floor(props.time / 10000) % 10000} />
+      <Digit digit={Math.floor(props.time / 1000) % 1000} />
+      <Digit digit={Math.floor(props.time / 100) % 100} />
+      <Digit digit={Math.floor(props.time / 10) % 10} />
+      <Digit digit={Math.floor(props.time % 10)} />
+    </div>
 	);
 };
 
